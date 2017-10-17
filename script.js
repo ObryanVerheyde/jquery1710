d38b2bc79c6228540b3dce86e47d6506b956153c
O'Bryan Verheyde <verheyde.obryan@gmail.com>
2017-10-17 11:57:06 +0200

    jquery exo

---
+++ script.js
@@ -0,0 +1,70 @@
+/*
+var p = document.createElement('p');
+var text = document.createTextNode('Mon super texte');
+// p.innerHTML = 'Montext';
+
+p.appendChild(text);
+
+var content = document.getElementById('content');
+content.appendChild(p);
+
+var body = document.getElementsByTagName('body');
+console.log(body);
+
+body[0].appendChild(text);
+
+var a = new Array([1]);
+var b = a;
+
+a[0] = 20;
+
+console.log(b);
+
+
+var $ = "hello";
+console.log($);
+*/
+
+
+
+$(document).ready(function() {
+  function colorTasks() {
+    $('li:even').css('backgroundColor', '#8DE6DB');
+  }
+
+  // $("#content").text("Mon texte");
+  $("#content").append("Mon texte");
+
+  var text = $('#task');
+  text.on('keypress', function(event) {
+    if(event.keyCode === 13) {
+      console.log(text.val());
+
+      var li = $(document.createElement('li'));
+      li.text(text.val());
+      li.hide().appendTo('#nav').slideDown(1000);
+
+      text.val(''); //Vide le champ du formulaire après l'entrée
+      colorTasks();
+    }
+  });
+
+  var list = $('li:odd'); //odd ou even
+  list.css('backgroundColor', 'yellow');
+
+  console.log(list);
+
+  $('.active').css('backgroundColor', 'blue');
+  $('.active').hover(function() {
+    $(this).addClass('hover');
+    $(this).css('backgroundColor', 'green');
+  }, function() {
+    $(this).css('backgroundColor', 'aquamarine')
+  });
+
+  $('li:even').css('backgroundColor', 'yellow');
+
+  $('#nav').on('click', 'li', function() {
+    $(this).css('backgroundColor', 'red');
+  });
+});
